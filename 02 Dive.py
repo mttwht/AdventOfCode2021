3d537fcd965261bb4ee1c65591eb2c9543a9c682
Matt <Matthew.White02@gmail.com>
2021-12-02 21:53:36 +0000

    Update to solve part 2

--- 02 Dive.py
+++ 02 Dive.py
@@ -2,16 +2,17 @@ file = open("res/02_input.txt", "r")
 
 steps = file.readlines()
 
-x = y = 0
+x = y = aim = 0
 
 for step in steps:
     (command, distance) = step.split()
     if command == "up":
-        y -= int(distance)
+        aim -= int(distance)
     elif command == "forward":
         x += int(distance)
+        y += aim * int(distance)
     elif command == "down":
-        y += int(distance)
+        aim += int(distance)
 
 print("Travelled", x, "forward and", y, "down")
 print("Answer", x*y)
