c28faf03f41ab5e9ad729250daebd2f922d2a520
Matt <Matthew.White02@gmail.com>
2021-12-04 22:00:51 +0000

    Update to solve part 2 Needs some serious refactoring but it works

--- 03 Binary disgnostic.py
+++ 03 Binary disgnostic.py
@@ -1,21 +1,42 @@
 with open("res/03_input.txt", "r") as file:
     lines = file.readlines()
 
-gamma = epsilon = ''
+oxygenGeneratorRatingLines = lines.copy()
+co2ScrubberRatingLines = lines.copy()
 
 for i in range(0, len(lines[0])-1):
     zeroes = ones = 0
-    for line in lines:
+    for line in oxygenGeneratorRatingLines:
         zeroes += 1 - int(line[i])
         ones += int(line[i])
-    if ones > zeroes:
-        gamma += '1'
-        epsilon += '0'
+    if ones >= zeroes:
+        temp = filter(lambda x: x[i] == '1', oxygenGeneratorRatingLines)
     else:
-        gamma += '0'
-        epsilon += '1'
-    
-print('gamma =', gamma, '=', int(gamma, 2))
-print('epsilon =', epsilon, '=', int(epsilon, 2))
+        temp = filter(lambda x: x[i] == '0', oxygenGeneratorRatingLines)
+    oxygenGeneratorRatingLines = list(temp)
+        
+    if len(oxygenGeneratorRatingLines) == 1:
+        break
 
-print(int(gamma, 2) * int(epsilon, 2))
+oxygenGeneratorRating = oxygenGeneratorRatingLines[0][:-1]
+
+for i in range(0, len(lines[0])-1):
+    zeroes = ones = 0
+    for line in co2ScrubberRatingLines:
+        zeroes += 1 - int(line[i])
+        ones += int(line[i])
+    if ones >= zeroes:
+        temp = filter(lambda x: x[i] == '0', co2ScrubberRatingLines)
+    else:
+        temp = filter(lambda x: x[i] == '1', co2ScrubberRatingLines)
+    co2ScrubberRatingLines = list(temp)
+        
+    if len(co2ScrubberRatingLines) == 1:
+        break
+
+co2ScrubberRating = co2ScrubberRatingLines[0][:-1]
+
+print('oxygenGeneratorRating =', oxygenGeneratorRating, '=', int(oxygenGeneratorRating, 2))
+print('co2ScrubberRating =', co2ScrubberRating, '=', int(co2ScrubberRating, 2))
+
+print(int(oxygenGeneratorRating, 2) * int(co2ScrubberRating, 2))
