69ee357f9ef98f04b68ba8d2477072f895508c55
Matt <Matthew.White02@gmail.com>
2021-12-07 22:45:14 +0000

    Update solution to use triangular number formula

--- 07 The Treachery of Whales.py
+++ 07 The Treachery of Whales.py
@@ -3,9 +3,12 @@ with open("res/07_input.txt", "r") as file:
 
 crabs = [int(x) for x in lines[0].split(",")]
 
+def triangle(x):
+    return x * (x + 1) // 2
+
 energies = []
 for i in range(0, max(crabs)+1):
-    energies.append(sum([sum(range(1, abs(x-i)+1)) for x in crabs]))
+    energies.append(sum([triangle(abs(x - i)) for x in crabs]))
 
 cheapest = min(energies)
 print(energies.index(cheapest), ":", cheapest)
