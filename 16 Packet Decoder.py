e2f236a8b2647846927be98cea0f99056d4e9969
Matt <Matthew.White02@gmail.com>
2021-12-17 23:37:09 +0000

    Update to solve part 2 WHEW! Would love to spend some time refactoring this in the future

--- 16 Packet Decoder.py
+++ 16 Packet Decoder.py
@@ -1,23 +1,36 @@
 with open("res/16_input.txt", "r") as file:
-    line = file.readline()
+    line = file.readline().strip()
 
-# line =  "D2FE28"						# v=6
-# line += "38006F45291200"				# v=1
-# line += "EE00D40C823060"				# v=7
-# line += "8A004A801A8002F478"			# v=16
-# line += "620080001611562C8802118E34"	# v=12
-# line += "C0015000016115A2E0802F182340"	# v=23
-# line += "A0016C880162017C3686B18A3D4780"# v=31
+# line =  "C200B40A82" # 1 + 2 = 3
+# line =  "04005AC33890" # 6 * 9 = 54
+# line =  "880086C3E88112" # min(7,8,9) = 7
+# line =  "CE00C43D881120" # max(7,8,9) = 9
+# line =  "D8005AC2A8F0" # 5 < 15 = 1
+# line =  "F600BC2D8F" # 5 > 15 = 0
+# line =  "9C005AC2F8F0" # 5 == 15 = 0
+# line =  "9C0141080250320F1802104A08" # 1 + 3 == 2 * 2 = 1
 
 LITERAL_TYPE = 4
+
+OPERATOR_SUM = 0
+OPERATOR_PRODUCT = 1
+OPERATOR_MIN = 2
+OPERATOR_MAX = 3
+OPERATOR_GREATERTHAN = 5
+OPERATOR_LESSTHAN = 6
+OPERATOR_EQUALTO = 7
+
 SUB_PACKET_LENGTH_TYPE = 0
 SUB_PACKET_NUMBER_TYPE = 1
 
 
 def hexStrToBinStr(s):
-    i = int(s, 16)
-    b = bin(i)[2:]
-    return b.zfill((len(s)-1)*4)
+    binStr = ""
+    for c in s:
+        i = int(c, 16)
+        b = bin(i)[2:]
+        binStr += b.zfill(4)
+    return binStr
 
 def getVersion(stream):
     version = int(stream[0:3], 2)
@@ -37,7 +50,7 @@ def parseLiteral(stream):
         result += group[1:]
         if group[0] == '0':
             processing = False
-    return packet, getVersion(packet)
+    return packet, int(result, 2)
 
 def getLengthTypeId(stream):
     b = stream[6]
@@ -53,7 +66,8 @@ def getSubPacketNumber(stream):
 
 def parseOperator(stream):
     lengthTypeId = getLengthTypeId(stream)
-    version = getVersion(stream)
+    
+    vals = []
     
     if lengthTypeId == SUB_PACKET_LENGTH_TYPE:
         length = getSubPacketLength(stream)
@@ -62,7 +76,7 @@ def parseOperator(stream):
         while len(subPacket) > 0:
             p,v = parseLiteral(subPacket) if getType(subPacket)==LITERAL_TYPE else parseOperator(subPacket)
             subPacket = subPacket[len(p):]
-            version += v
+            vals.append(v)
         packet = stream[:totalPacketBitLength]
     elif lengthTypeId == SUB_PACKET_NUMBER_TYPE:
         number = getSubPacketNumber(stream)
@@ -72,20 +86,31 @@ def parseOperator(stream):
             p,v = parseLiteral(subPacket) if getType(subPacket)==LITERAL_TYPE else parseOperator(subPacket)
             packet += p
             subPacket = subPacket[len(p):]
-            version += v
+            vals.append(v)
     
-    return packet, version
-        
+    operator = getType(packet)
+    if operator == OPERATOR_SUM: val = sum(vals)
+    elif operator == OPERATOR_PRODUCT:
+        val = vals[0]
+        for i in range(1, len(vals)): val *= vals[i]
+    elif operator == OPERATOR_MIN: val = min(vals)
+    elif operator == OPERATOR_MAX: val = max(vals)
+    elif operator == OPERATOR_GREATERTHAN: val = 1 if vals[0] > vals[1] else 0
+    elif operator == OPERATOR_LESSTHAN: val = 1 if vals[0] < vals[1] else 0
+    elif operator == OPERATOR_EQUALTO: val = 1 if vals[0] == vals[1] else 0
+    
+    return packet, val
+
 
 def getPacket(stream):
     packetType = getType(stream)
     
     if packetType == LITERAL_TYPE:
-        packet, version = parseLiteral(stream)
+        packet, val = parseLiteral(stream)
     else:
-        packet, version = parseOperator(stream)
+        packet, val = parseOperator(stream)
         
-    return packet, version
+    return packet, val
 
 def hexPadPacket(packet, stream):
     # clean up any incomplete hex characters
@@ -101,10 +126,7 @@ inputStream = hexStrToBinStr(line)
 
 totalVersion = 0
 while len(inputStream) > 0:
-    packet,version = getPacket(inputStream)
+    packet,val = getPacket(inputStream)
     packet = hexPadPacket(packet,inputStream)
     inputStream = inputStream[len(packet):]
-    totalVersion += version
-    print("{0}\t{1}".format(hex(int(packet, 2)), version))
-
-print("totalVersion","=",totalVersion)
+    print("{0}\t{1}".format(hex(int(packet, 2)), val))
